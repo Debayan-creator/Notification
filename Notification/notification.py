6cd8a46b54bdd69915a095a91615f251af35cc65
Debayan <73572191+Debayan-creator@users.noreply.github.com>
2022-04-20 15:54:45 +0530

    Add files via upload

---
+++ Notification/notification.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+if __name__=="__main__":
+    notification.notify(
+        title="PLease Drink Water",
+        message="Bsdk.....",
+        timeout=10
+    )
+    time.sleep(60*60)
